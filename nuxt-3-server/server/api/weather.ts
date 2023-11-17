a3765418a10b1c852b9c9fa11c8a7a369b2692eb
Kumori <tauen8@gmail.com>
2023-11-16 20:16:51 -0600

    restructured files to expose root and hopefull deploy

--- nuxt-3-server/server/api/weather.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async event => {
-
-
-    const response = await $fetch("../src/DnD.png")
-
-
-//$fetch must be used since this only serverside API
-    return response
-})
